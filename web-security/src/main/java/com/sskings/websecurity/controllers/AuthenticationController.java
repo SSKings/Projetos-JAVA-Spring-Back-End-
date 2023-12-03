2ab1cfcebdc7757d8941f2478520d3d10b2a7108
sskings <sergiosantanadosreis@hotmail.com>
2023-12-03 17:37:46 -0300

    melhorias

--- web-security/src/main/java/com/sskings/websecurity/controllers/AuthenticationController.java
+++ web-security/src/main/java/com/sskings/websecurity/controllers/AuthenticationController.java
@@ -5,11 +5,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.sskings.websecurity.models.User;
 import com.sskings.websecurity.repositories.UserRepository;
@@ -56,15 +56,16 @@ public class AuthenticationController {
 	}
 	
 	@PostMapping("/register")
-    public String registerUser(@RequestParam("username") String username,
-    		@RequestParam("password") String password, Model model) {
+    public ModelAndView registerUser(@RequestParam("username") String username,
+    		@RequestParam("password") String password) {
 		
 		String encryptedPassword = new BCryptPasswordEncoder().encode(password);
 		System.out.println("Username: " + username);
 		System.out.println("Senha criptografada: " + encryptedPassword);
         User newUser = new User(username, encryptedPassword);
         userRepository.save(newUser);
-        model.addAttribute("registrationSuccess", "registrado com sucesso.");
-        return "redirect:/login";
+        var modelAndView = new ModelAndView("/login");
+        modelAndView.addObject("registrationSuccess", "registrado com sucesso.");
+        return modelAndView;
     }
 }
