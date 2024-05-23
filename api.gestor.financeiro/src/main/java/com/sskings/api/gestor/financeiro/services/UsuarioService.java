b163ebd9eee4b432b8ce21510b0da15b62504e93
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-05-23 11:21:19 -0300

    serviços do usuário.

---
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/services/UsuarioService.java
@@ -0,0 +1,41 @@
+package com.sskings.api.gestor.financeiro.services;
+
+import com.sskings.api.gestor.financeiro.models.UsuarioModel;
+import com.sskings.api.gestor.financeiro.repositories.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public UsuarioModel save(UsuarioModel usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public List<UsuarioModel> findAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<UsuarioModel> findById(UUID id){
+        return usuarioRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(UsuarioModel usuario){
+        usuarioRepository.delete(usuario);
+    }
+
+    public boolean existByEmail(String email){
+        return usuarioRepository.existByEmail(email);
+    }
+
+}
