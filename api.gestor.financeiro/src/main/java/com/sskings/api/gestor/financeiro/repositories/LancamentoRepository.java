07c346db7e35c19dc3a4d7621083144a1bdc9a7b
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-05-23 15:51:31 -0300

    ajuste

--- api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/repositories/LancamentoRepository.java
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/repositories/LancamentoRepository.java
@@ -1,10 +1,11 @@
 package com.sskings.api.gestor.financeiro.repositories;
 
+import com.sskings.api.gestor.financeiro.models.LancamentoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface LancamentoRepository extends JpaRepository<LancamentoRepository, UUID> {
+public interface LancamentoRepository extends JpaRepository<LancamentoModel, UUID> {
 }
