33306ab4d22b6ddf57ff73827ba156e1fe7b431d
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-05-25 17:51:02 -0300

    Dto do tipo Record para Response de Conta

---
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/dto/ContaResponseDto.java
@@ -0,0 +1,12 @@
+package com.sskings.api.gestor.financeiro.dto;
+
+import com.sskings.api.gestor.financeiro.models.ContaModel;
+
+import java.math.BigDecimal;
+
+public record ContaResponseDto(long numero, String banco, BigDecimal saldo) {
+
+    public ContaResponseDto(ContaModel contaModel){
+        this(contaModel.getNumero(), contaModel.getBanco(), contaModel.getSaldo());
+    }
+}
