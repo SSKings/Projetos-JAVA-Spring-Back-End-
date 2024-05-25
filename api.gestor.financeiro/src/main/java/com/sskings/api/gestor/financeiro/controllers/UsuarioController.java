ad5bb1d0e4b7c5b23e554f33923234ab01e0be32
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-05-25 13:14:51 -0300

    ajuste

--- api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/controllers/UsuarioController.java
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/controllers/UsuarioController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/usuario")
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
