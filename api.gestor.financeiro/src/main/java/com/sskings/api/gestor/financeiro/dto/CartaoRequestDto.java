3524f14a2dc705182f887d251a85e43ea78c94bc
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-05-28 13:39:05 -0300

    Dtos de Cartao

---
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/dto/CartaoRequestDto.java
@@ -0,0 +1,10 @@
+package com.sskings.api.gestor.financeiro.dto;
+
+import com.sskings.api.gestor.financeiro.models.CartaoModel;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record CartaoRequestDto(long numero, String banco, LocalDate vencimento, UUID usuario_id, BigDecimal limite) {
+}
---
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/dto/CartaoResponseDto.java
@@ -0,0 +1,13 @@
+package com.sskings.api.gestor.financeiro.dto;
+
+import com.sskings.api.gestor.financeiro.models.CartaoModel;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record CartaoResponseDto(long numero, String banco, LocalDate vencimento, BigDecimal limite) {
+
+    public CartaoResponseDto(CartaoModel cartaoModel){
+        this(cartaoModel.getNumero(), cartaoModel.getBanco(), cartaoModel.getVencimento(), cartaoModel.getLimite());
+    }
+}
