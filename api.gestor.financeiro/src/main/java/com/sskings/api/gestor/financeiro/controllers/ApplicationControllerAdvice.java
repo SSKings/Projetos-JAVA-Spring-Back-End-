cc108b120245e8f4e4ed69a1f80538ac772b916f
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-05-31 21:44:05 -0300

    ControllerAdvice e ExceptionHandler

---
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/controllers/ApplicationControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.sskings.api.gestor.financeiro.controllers;
+
+import com.sskings.api.gestor.financeiro.errors.ApiErrors;
+import com.sskings.api.gestor.financeiro.exception.RegraNegocioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex){
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
---
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/exception/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package com.sskings.api.gestor.financeiro.exception;
+
+public class RegraNegocioException extends RuntimeException{
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
