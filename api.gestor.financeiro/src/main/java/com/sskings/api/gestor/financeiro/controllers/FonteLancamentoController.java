ac93f31d5f434059acb13994488c9a05f709679b
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-06-16 20:25:40 -0300

    controllers de Fonte e Tipo

---
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/controllers/FonteLancamentoController.java
@@ -0,0 +1,38 @@
+package com.sskings.api.gestor.financeiro.controllers;
+
+import com.sskings.api.gestor.financeiro.dto.lancamento.fonte.FonteRequestDto;
+import com.sskings.api.gestor.financeiro.dto.lancamento.fonte.FonteResponseDto;
+import com.sskings.api.gestor.financeiro.services.FonteLancamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/lancamentos/fonte")
+@RequiredArgsConstructor
+public class FonteLancamentoController {
+
+    private final FonteLancamentoService fonteLancamentoService;
+
+    @PostMapping
+    public ResponseEntity<FonteResponseDto> save(@RequestBody FonteRequestDto fonteRequestDto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(fonteLancamentoService.save(fonteRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FonteResponseDto>> getAll(){
+        return ResponseEntity.ok(fonteLancamentoService.findAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable UUID id){
+        fonteLancamentoService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/controllers/TipoLancamentoController.java
@@ -0,0 +1,38 @@
+package com.sskings.api.gestor.financeiro.controllers;
+
+import com.sskings.api.gestor.financeiro.dto.lancamento.tipo.TipoRequestDto;
+import com.sskings.api.gestor.financeiro.dto.lancamento.tipo.TipoResponseDto;
+import com.sskings.api.gestor.financeiro.services.TipoLancamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/lancamentos/tipo")
+@RequiredArgsConstructor
+public class TipoLancamentoController {
+
+    private final TipoLancamentoService tipoLancamentoService;
+
+    @PostMapping
+    public ResponseEntity<TipoResponseDto> save(@RequestBody TipoRequestDto tipoRequestDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(tipoLancamentoService.save(tipoRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TipoResponseDto>> findAll() {
+        return ResponseEntity.ok(tipoLancamentoService.findAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable UUID id) {
+        tipoLancamentoService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
