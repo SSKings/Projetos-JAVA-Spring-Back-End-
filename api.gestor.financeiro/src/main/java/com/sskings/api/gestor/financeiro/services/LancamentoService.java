96be921ac3be6f1e7c133b79196b6b68234e6feb
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-06-18 14:09:20 -0300

    ajuste

--- api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/services/LancamentoService.java
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/services/LancamentoService.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
