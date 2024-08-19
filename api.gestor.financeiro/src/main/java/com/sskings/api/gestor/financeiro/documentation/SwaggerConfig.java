87e6e5884a4d65b982f6bde3ebd735f7f64ed201
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-08-19 16:52:41 -0300

    melhorias

--- api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/documentation/SwaggerConfig.java
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/documentation/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.sskings.api.gestor.financeiro.documentation;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -29,11 +30,21 @@ public class SwaggerConfig {
                                         .scheme(scheme)
                         )
                 )
-                .info(new Info()
-                        .title("Gestor Financeiro - Rest API")
-                        .description("Exemplo de API RESTful e conhecimentos do Framework Spring")
-                        .version("1.0")
+                .info(apiInfo());
+    }
 
-                );
+    public Info apiInfo(){
+        return new Info()
+                .title("Gestor Financeiro - Rest API")
+                .description("Exemplo de API RESTful e conhecimentos do Framework Spring")
+                .version("1.0")
+                .contact(contact());
+    }
+
+    public Contact contact(){
+        return new Contact()
+                .name("Sérgio Santana dos Reis")
+                .email("sergiosantanadosreis@gmail.com")
+                .url("https://www.linkedin.com/in/sskings/");
     }
 }
