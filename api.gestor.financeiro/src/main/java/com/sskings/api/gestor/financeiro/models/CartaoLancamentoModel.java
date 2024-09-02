b45516fc29d7f5d90ee03946302a1040061605a9
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-09-02 16:31:56 -0300

    refactor: criando construtor especifico

--- api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/models/CartaoLancamentoModel.java
+++ api.gestor.financeiro/src/main/java/com/sskings/api/gestor/financeiro/models/CartaoLancamentoModel.java
@@ -8,14 +8,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
 @Entity
 @DiscriminatorValue("CREDITO")
-@NoArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class CartaoLancamentoModel extends LancamentoModel{
 
     @ManyToOne
     @JoinColumn(name = "cartao_id")
     private CartaoModel cartao;
+
+    public CartaoLancamentoModel(UUID id, BigDecimal valor, LocalDate data, UsuarioModel usuario, TipoLancamentoModel tipo, FonteLancamentoModel fonte, CartaoModel cartao){
+        super(id,valor,data,usuario,tipo,fonte);
+        this.cartao = cartao;
+    }
 }
