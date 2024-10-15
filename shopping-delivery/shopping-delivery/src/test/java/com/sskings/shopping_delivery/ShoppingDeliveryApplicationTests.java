fcc442d688bc84f745f3af2eb32ea07e0d6f6c77
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-10-15 09:01:30 -0300

    começando projeto + diagrama EER

---
+++ shopping-delivery/shopping-delivery/src/test/java/com/sskings/shopping_delivery/ShoppingDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sskings.shopping_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
