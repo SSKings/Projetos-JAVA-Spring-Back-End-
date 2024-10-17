affc7ca61622cc619ee2189051ba29c3170cfe7b
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-10-17 15:33:14 -0300

    recursos: - add método findByNome - add método findByCpf - add método findByIdWithEnderecos - add método findByIdWithPedidos

--- shopping-delivery/shopping-delivery/src/main/java/com/sskings/shopping_delivery/repositories/ClienteRepository.java
+++ shopping-delivery/shopping-delivery/src/main/java/com/sskings/shopping_delivery/repositories/ClienteRepository.java
@@ -2,6 +2,21 @@ package com.sskings.shopping_delivery.repositories;
 
 import com.sskings.shopping_delivery.models.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+
+    Optional<ClienteModel> findByNome(String nome);
+
+    Optional<ClienteModel> findByCpf(String cpf);
+
+    @Query("SELECT c FROM ClienteModel c JOIN FETCH c.enderecos WHERE c.id = :id")
+    Optional<ClienteModel> findByIdWithEnderecos(@Param("id") Long id);
+
+    @Query("SELECT c FROM ClienteModel c JOIN FETCH c.pedidos WHERE c.id = :id")
+    Optional<ClienteModel> findByIdWithPedidos(@Param("id") Long id);
+
 }
