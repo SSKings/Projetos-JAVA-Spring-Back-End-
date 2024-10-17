cdf717fa5aff755849ad02eaf2b1de9f418c01f7
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-10-17 15:55:58 -0300

    testes: - add arquivo de configuração .yml - criado classe de testes do repositório de Cliente

---
+++ shopping-delivery/shopping-delivery/src/test/java/com/sskings/shopping_delivery/repositories/ClienteRepositoryTest.java
@@ -0,0 +1,102 @@
+package com.sskings.shopping_delivery.repositories;
+
+import com.sskings.shopping_delivery.models.ClienteModel;
+import com.sskings.shopping_delivery.models.EnderecoModel;
+import com.sskings.shopping_delivery.models.PedidoModel;
+import com.sskings.shopping_delivery.models.StatusPedido;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+class ClienteRepositoryTest {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private ClienteModel cliente0;
+
+    @BeforeEach
+    public void setup(){
+
+        cliente0 = new ClienteModel(
+                null,
+                "Sergio",
+                "sergiosantanadosreis@gmail.com",
+                "00-00000-0000",
+                "000.000.000.00",
+                LocalDateTime.now(), null, null);
+
+        EnderecoModel endereco0 = new EnderecoModel(
+                null, "Rua A", "11B", "Bairro A", "Casa",
+                LocalDateTime.now(), cliente0 );
+
+        PedidoModel pedido0 = new PedidoModel(
+                null, cliente0, endereco0, LocalDateTime.now(), StatusPedido.PENDENTE, BigDecimal.valueOf(100.00), null);
+
+        cliente0.setEnderecos(List.of(endereco0));
+        cliente0.setPedidos(List.of(pedido0));
+        clienteRepository.save(cliente0);
+        entityManager.flush();
+
+
+    }
+
+    // test[System Under Test]_[Condition or State Change]_[Expected Result]
+    @DisplayName("Dado Nome Do Cliente Quando FindByNome Retornar Objeto ClienteModel")
+    @Test
+    void testDadoNomeDoCliente_quandoFindByNome_retornarObjetoClienteModel() {
+        // Given / Arrange
+        // When / Act
+        ClienteModel clienteRetornado = clienteRepository.findByNome(cliente0.getNome()).get();
+        // Then / Assert
+        assertNotNull(clienteRetornado);
+        assertEquals(cliente0.getNome(), clienteRetornado.getNome());
+
+    }
+
+    // test[System Under Test]_[Condition or State Change]_[Expected Result]
+    @DisplayName("Dado Id Do Cliente Quando FindByIdWithEnderecos Retorne ClienteModel Com Enderecos")
+    @Test
+    void testDadoIdDoCliente_QuandoFindByIdWithEnderecos_RetorneClienteModelComEnderecos() {
+        // Given / Arrange
+
+        // When / Act
+        ClienteModel clienteRetornado = clienteRepository.findByIdWithEnderecos(cliente0.getId()).get();
+        // Then / Assert
+        assertNotNull(clienteRetornado);
+        assertNotNull(clienteRetornado.getEnderecos());
+        assertEquals(clienteRetornado.getEnderecos().size(), 1);
+        assertNotNull(clienteRetornado.getEnderecos().get(0).getId());
+        assertEquals("Rua A", clienteRetornado.getEnderecos().get(0).getLogradouro());
+    }
+
+    @DisplayName("Dado Id Do Cliente Quando FindByIdWithPedidos Retorne ClienteModel Com Pedidos")
+    @Test
+    void testDadoIdDoCliente_QuandoFindByIdWithPedidos_RetorneClienteModelComPedidos() {
+        // Given / Arrange
+
+        // When / Act
+        ClienteModel clienteRetornado = clienteRepository.findByIdWithPedidos(cliente0.getId()).get();
+        // Then / Assert
+        assertNotNull(clienteRetornado);
+        assertNotNull(clienteRetornado.getPedidos());
+        assertEquals(clienteRetornado.getPedidos().size(), 1);
+        assertNotNull(clienteRetornado.getPedidos().get(0).getId());
+        assertEquals(BigDecimal.valueOf(100.00), clienteRetornado.getPedidos().get(0).getTotal());
+    }
+
+}
