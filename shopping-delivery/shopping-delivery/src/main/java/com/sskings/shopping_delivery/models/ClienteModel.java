13dfbe1d4e3716aa376f95848cc874c271616047
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-10-18 11:49:42 -0300

    modificação: - modificado para unique atributos email e cpf da classe ClienteModel.

--- shopping-delivery/shopping-delivery/src/main/java/com/sskings/shopping_delivery/models/ClienteModel.java
+++ shopping-delivery/shopping-delivery/src/main/java/com/sskings/shopping_delivery/models/ClienteModel.java
@@ -22,13 +22,13 @@ public class ClienteModel {
 
     @Column(nullable = false, length = 100)
     private String nome;
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, unique = true)
     private String email;
 
     @Column(nullable = false, length = 15)
     private String telefone;
 
-    @Column(nullable = false, length = 15)
+    @Column(nullable = false, length = 15, unique = true)
     private String cpf;
 
     @Column(name = "data_cadastro", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
