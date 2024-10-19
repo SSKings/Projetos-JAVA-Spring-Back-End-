ff3fcff288085789403cc2dbfa1808b178269c23
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-10-18 21:40:07 -0300

    ajuste

--- shopping-delivery/shopping-delivery/src/test/java/com/sskings/shopping_delivery/ShoppingDeliveryApplicationTests.java
+++ shopping-delivery/shopping-delivery/src/test/java/com/sskings/shopping_delivery/ShoppingDeliveryApplicationTests.java
@@ -2,8 +2,10 @@ package com.sskings.shopping_delivery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ShoppingDeliveryApplicationTests {
 
 	@Test
