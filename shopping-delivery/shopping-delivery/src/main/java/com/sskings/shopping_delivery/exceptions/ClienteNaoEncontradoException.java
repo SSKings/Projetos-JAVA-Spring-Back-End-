ee0f2d7892ccd09a08a11243e74e7dc8f8d0bf23
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-11-26 13:22:27 -0300

    recurso: - classe de exceção para Cliente Não Encontrado.

---
+++ shopping-delivery/shopping-delivery/src/main/java/com/sskings/shopping_delivery/exceptions/ClienteNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.sskings.shopping_delivery.exceptions;
+
+public class ClienteNaoEncontradoException extends RuntimeException {
+    public ClienteNaoEncontradoException(String message) {
+        super(message);
+    }
+}
