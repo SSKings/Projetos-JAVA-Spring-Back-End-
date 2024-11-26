55297a5318f66646516c2af61eab3482176b28fc
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-11-25 21:12:56 -0300

    recurso: - add Classe de Exceção para CPf Existente.

---
+++ shopping-delivery/shopping-delivery/src/main/java/com/sskings/shopping_delivery/exceptions/CpfExistenteException.java
@@ -0,0 +1,7 @@
+package com.sskings.shopping_delivery.exceptions;
+
+public class CpfExistenteException extends RuntimeException {
+    public CpfExistenteException(String message) {
+        super(message);
+    }
+}
