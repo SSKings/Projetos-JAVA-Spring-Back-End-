99f38cbf318af7d4cfe519565d1da416ae9e367f
Sérgio <95772420+SSKings@users.noreply.github.com>
2024-12-16 17:41:26 -0300

    test da página do swagger ui

--- shopping-delivery/shopping-delivery/src/test/java/com/sskings/shopping_delivery/integrationtests/swagger/SwaggerIntegrationTest.java
+++ shopping-delivery/shopping-delivery/src/test/java/com/sskings/shopping_delivery/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -1,15 +1,34 @@
 package com.sskings.shopping_delivery.integrationtests.swagger;
 
+import com.sskings.shopping_delivery.config.TestConfigs;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 class SwaggerIntegrationTest {
 
+	@DisplayName("Deve Mostrar A Página Do Swagger UI")
 	@Test
-	void contextLoads() {
+	void deveMostrarAPaginaDoSwaggerUI() {
+		var content = given()
+				.baseUri("http://localhost:8080/swagger-ui/index.html")
+				.port(TestConfigs.SERVER_PORT)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+
+		Assertions.assertTrue(content.contains("Swagger UI"));
+
 	}
 
 }
