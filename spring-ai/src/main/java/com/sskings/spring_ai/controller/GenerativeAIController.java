555e33e1b9b2a00221f5742aa72e25075066d8d4
Sérgio <95772420+SSKings@users.noreply.github.com>
2025-05-13 23:15:30 -0300

    Usando PromptTemplate (gerador de receitas)

--- spring-ai/src/main/java/com/sskings/spring_ai/controller/GenerativeAIController.java
+++ spring-ai/src/main/java/com/sskings/spring_ai/controller/GenerativeAIController.java
@@ -3,6 +3,8 @@ package com.sskings.spring_ai.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sskings.spring_ai.service.ChatService;
+import com.sskings.spring_ai.service.RecipeService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GenerativeAIController {
 
     private final ChatService chatService;
+    private final RecipeService recipeService;
 
-    public GenerativeAIController(ChatService chatService){
+    public GenerativeAIController(ChatService chatService, RecipeService recipeService){
         this.chatService = chatService;
+        this.recipeService = recipeService;
     }
     
     @GetMapping("ask-ai")
@@ -26,5 +30,12 @@ public class GenerativeAIController {
         return chatService.getResponseWithOptions(prompt);
     }
     
+    @GetMapping("recipe-creator")
+    public String recipeCreator(@RequestParam String ingredients, 
+                                @RequestParam(defaultValue = "any") String cuisine, 
+                                @RequestParam(defaultValue = "none") String dietaryRestrictions) {
+        
+            return recipeService.createRecipe(ingredients, cuisine, dietaryRestrictions);
+    }
     
 }
